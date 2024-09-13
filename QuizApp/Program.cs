e75baf71651078920f041395ae10bd8c85879acf
kaloy <kaloy@MSI>
2024-09-13 12:09:30 +0300

    Add project files.

---
+++ QuizApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace QuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Question[] myQuestions = new Question[]
+            {
+                new Question("What is the capital of germany ?",
+                new string[] {"Paris","Berlin","Madrid","London"},
+                1
+                ),
+                new Question("What is the 2 + 2 ?",
+                new string[] {"3","4","5","6"},
+                1
+                ),
+                new Question("Who wrote Hamlet ?",
+                new string[] {"Bethoven","Ivan Vazov","Shakespeare","Kalata"},
+                2
+                ),
+                new Question("What is the capital of germany",
+                new string[] {"Paris","Berlin","Madrid","London"},
+                1
+                )
+            };
+
+            Quiz myQuiz = new Quiz(myQuestions);
+
+            myQuiz.StartQuiz();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QuizApp/Quiz.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    internal class Quiz
+    {
+        private Question[] _questions;
+        private int _score;
+
+        public Quiz(Question[] questions)
+        {
+            this._questions = questions;
+            _score = 0;
+        }
+
+
+        public void StartQuiz()
+        {
+            Console.WriteLine("Welcome to the Quiz");
+            int questionNumber = 1;
+            foreach (Question question in _questions)
+            {
+                Console.WriteLine($"Question: {questionNumber++}");
+                DisplayQuestion(question);
+                int userChoice = GetUserChoice();
+                if (question.IsCorrectAnswer(userChoice))
+                {
+                    Console.WriteLine("Correct");
+                    _score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong! The correct answer was : {question.Answers[question.CorrectAnswerIndex]}");
+                }
+            }
+            DisplayResults();
+
+        }
+
+        private void DisplayResults()
+        {
+            Console.WriteLine($"Quiz finished your score is {_score} out of {_questions.Length}");
+
+            double percentage = (double)_score / _questions.Length;
+
+            if (percentage >= 0.8) 
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Excellent");
+            }else if (percentage >= 0.5)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Good effor");
+            }else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("More excersise");
+            }
+        }
+
+        private void DisplayQuestion(Question question)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(question.QuestionText);
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("  ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine($". {question.Answers[i]}");
+            }
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Your answer (number):");
+            string input = Console.ReadLine();
+            int choice = 0;
+
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 4) {
+                Console.WriteLine("Invalid choice. Please enter a number between 1 and 4:");
+                input = Console.ReadLine();
+            }
+            return choice -1;
+        }
+
+    } 
+}
